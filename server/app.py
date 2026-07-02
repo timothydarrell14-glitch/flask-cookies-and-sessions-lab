0ca6858739ef5fa16c8406d285bd0caf18409e71
TimDev14 <njagi.timothy14@gmail.com>
2026-07-02 12:33:03 +0300

    Fix typo in session variable name for page views in show_article function

--- server/app.py
+++ server/app.py
@@ -27,13 +27,14 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-    session['page_vis'] = session.get('page_views', 0) + 1
+    session['page_views'] = session.get('page_views', 0) + 1
 
     if session['page_views'] <= 3:
         article = Article.query.get(id)
         return make_response(ArticleSchema().dump(article), 200)
     else:
         return jsonify({'message' : 'Maximum pageview limit reached'}), 401
+
 
 
 if __name__ == '__main__':
